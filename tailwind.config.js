8ef897257c704ccfc8041d6c00284d4f043d4478
Shayan Payandeh <shayan.iker@gmail.com>
2023-03-20 02:21:28 +0330

    changes to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      // animation: {
-      //   bounce: 'bounce 2s linear',
-      // },
       fontFamily: {
         caveat: ['Caveat', 'sans-serif'],
         dmSerif: ['Tapestry', 'sans-serif'],
